1c9796c8b0f3cec95ac0c146841455480fa51ea7
Roni Juppi <roni.juppi@gmail.com>
2017-03-01 22:57:38 +0200

    setup server, so it sends any js files included in the html as well

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
-var app = require('express')()
-var http = require('http').Server(app)
+const app = require('express')()
+const http = require('http')
+const gameport = process.env.PORT || 3000
+const io = require('socket.io')
+const UUID = require('node-uuid')
+const server = http.Server(app)
 
+const verbose = true;
+
+server.listen(gameport)
+
+console.log('listening on *:3000')
+
+// express setup
 app.get('/', function(req, res){
-  res.sendFile(__dirname + "/index.html")
-});
+    //send the index.html for the client
+    console.log('trying to load %s', __dirname + '/index.html');
+    res.sendFile(__dirname + "/index.html")
+})
 
-http.listen(3000, function(){
-  console.log('listening on *:3000')
-});
+app.get( '/*' , function( req, res, next ) {
+    //send any other files the client asks for (like any .js files etc.)
+    var file = req.params[0];
+    if(verbose) console.log('file requested: ' + file);
+    res.sendFile( __dirname + '/' + file);
+})
+
+// socket.io setup
+const sio = io.listen(server)
+
+sio.sockets.on('connection', function (client) {
+    client.id = UUID()
+    client.emit('onconnected', { id: client.id } )
+
+    console.log('client: ' + client.id + ', connected')
+
+    client.on('message', function(message) {
+        console.log('clientid: ' + client.id + ', message: ' + message)
+    })
+
+    client.on('disconnect', function () {
+        console.log('client disconnected; ' + client.id)
+    })
+})
